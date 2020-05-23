72a570c3ce9b19f0b4af3ba485f36fdf778e64e3
vinitmodi.dev <vinitmodi.dev@gmail.com>
2020-05-24 01:09:14 +0530

    Custom validator through function

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,14 +1,18 @@
 from django import forms
 from django.core import validators
 
-def check_for_v(value):
-    if value[0].lower() != 'v':
-        raise forms.ValidationError("Name should start with v")
 
 class FormName(forms.Form):
-    name = forms.CharField(validators=[check_for_v])
+    name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label="Enter your email again")
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False, 
-                                 widget=forms.HiddenInput,
-                                 validators=[validators.MaxLengthValidator(0)])
+    
+
+    def clean(self):
+        clean_all_data = super().clean()
+        email = clean_all_data['email']
+        verify_email = clean_all_data['verify_email']
+
+        if email != verify_email:
+            raise forms.ValidationError("Please make sure entered emails are match")
