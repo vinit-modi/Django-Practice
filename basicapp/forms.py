7d0da046e069205e48f77fc6de80034b57e7538c
vinitmodi.dev <vinitmodi.dev@gmail.com>
2020-05-24 00:45:16 +0530

    Custom Validators

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from django.core import validators
 
+def check_for_v(value):
+    if value[0].lower() != 'v':
+        raise forms.ValidationError("Name should start with v")
+
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_v])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.CharField(required=False, 
