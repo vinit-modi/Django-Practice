c3f3a93f7f0029aaad9b813a52a5be9cccb7d0a2
vinitmodi.dev <vinitmodi.dev@gmail.com>
2020-05-24 00:30:06 +0530

    form Validation

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -4,4 +4,11 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False, widget=forms.HiddenInput)
+
+    def clean_botcatcher(self):
+        data = self.cleaned_data["botcatcher"]
+        if len(data) > 0:
+            raise forms.ValidationError("GOTCHA BOT")
+        return data
     
